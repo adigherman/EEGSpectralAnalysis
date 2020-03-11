f84ef05048948be498c8761f81fc6bc597763a6f
adigherman <adig@jhu.edu>
2020-03-11 16:02:48 -0400

    power_eeg fully implemented

--- R/power_eeg.R
+++ R/power_eeg.R
@@ -4,7 +4,7 @@
 #'   power and band aggregation as well as relative
 #'   band power and aggregation
 #'   
-#' @param power_spect matrix containing the square of the Fourier
+#' @param power_spectrum matrix containing the square of the Fourier
 #'   coefficients for an EEG data set. Each column corresponds to 
 #'   a 5 second interval window. Each column corresponds to a 
 #'   frequency. Lowest frequency is 1/num_sec_w, typically 0.2Hz
@@ -44,12 +44,42 @@
 #'                           this aggregates power in a number of
 #'                           aggreg_level adjacent intervals of length
 #'                           num_sec_w
-#'                           
+#'        
+#' @importFrom RcppRoll roll_mean  
+#' @importFrom pracma repmat                   
 #' @export
-power_eeg = function(power_spect,
+power_eeg = function(power_spectrum,
                      freq_min = 0.8,
                      freq_max = 4,
                      num_sec_w = 5,
                      aggreg_level = 6) {
   
+  # Define the start and end index of the spectral window
+  f1 <- floor(freq_min*num_sec_w) + 1     
+  f2 <- floor(freq_max*num_sec_w) + 1
+  
+  #Find the length of the vectors for the aggregation in larger temporal windows
+  length_aggreg=floor(ncol(power_spectrum)/aggreg_level);
+  
+  #Obtain the absolute power in the particular spectral window
+  absolute_power <- power_spectrum[f1:f2, ];
+  absolute_band_power <- colSums(absolute_power);
+  
+  #Obtain absolute power in aggregated windows
+  absolute_band_aggreg <- roll_mean(absolute_band_power, n=aggreg_level, by=aggreg_level)
+  
+  #Obtain the relative power in the particular spectral window
+  aa <- colSums(power_spectrum);
+  rep_aa <- repmat(aa, nrow(power_spectrum),1)
+  normalized_aa <- power_spectrum/rep_aa;
+  relative_power <- normalized_aa[f1:f2, ];
+  relative_band_power <- colSums(relative_power);
+  
+  #Obtain absolute power in aggregated windows
+  relative_band_aggreg <- roll_mean(relative_band_power, n=aggreg_level, by=aggreg_level)
+  
+  output <- list(absolute_band_power,absolute_band_aggreg,relative_band_power,relative_band_aggreg)
+  names(output) <- c("absolute_band_power","absolute_band_aggreg","relative_band_power","relative_band_aggreg")
+  
+  return(output)
 }
--- README.md
+++ README.md
@@ -9,7 +9,9 @@ You can install `EEGSpectralAnalysis` from github with:
 remotes::install_github("adigherman/EEGSpectralAnalysis")
 ```
 
-## Usage
+## Usage Examples
+
+### Calculate absolute and power spectrum
 
 To calculate absolute and power spectrum as well as estimated and lowest frequencies for an EEG signal we will use the `fft_eeg()` function. The parameters of the function are:
 
@@ -29,4 +31,25 @@ List of 6
  $ num_seconds_window   : num 5
  $ sampling_frequency   : num 125
 ```
- 
+
+### Calculate absolute and relative band power
+
+To calculate absolute and and relative band power in a defined frequency band we will use the `power_eeg()` function. The parameters of the function are:
+
+* `power_spectrum` -  matrix containing the square of the Fourier coefficients for an EEG data set (returned by the `fft_eeg()` function. Each column corresponds to a 5 second interval window. Each column corresponds to a frequency. Lowest frequency is 1/num_sec_w, typically 0.2Hz;
+* `freq_min` - minimum frequency defining the frequency band of interest;
+* `freq_max maximum` - maximum frequency defining the frequency band of interest;
+* `num_sec_w` - number of seconds in a time window used to obtain the Fourier coefficients. Typically, this number is 5;
+* `aggreg_level` - number of 5 second intervals used to aggregate power. Typically, this number is 6 to ensure a 30 second interval window (standard in EEG analysis).
+
+``` {r}
+> eeg_filepath <- system.file("extdata", "EEG10009_v1.csv.gz", package = "EEGSpectralAnalysis")
+> eeg_params <- fft_eeg(eeg_filepath)
+> power_eeg_params <- power_eeg(eeg_params$power_spectrum)
+> str(power_eeg_params)
+List of 4
+ $ absolute_band_power : num [1:6474] 5443549 23583271 16321002 4640000 8043634 ...
+ $ absolute_band_aggreg: num [1:1079] 10640555 8014268 14069995 6132555 11820995 ...
+ $ relative_band_power : num [1:6474] 0.408 0.755 0.751 0.328 0.469 ...
+ $ relative_band_aggreg: num [1:1079] 0.541 0.483 0.586 0.449 0.553 ...
+ ```
